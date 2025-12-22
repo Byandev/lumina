8e22a8f6bc42c595b6289da1f571612297313ee5
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-12-22 20:11:58 +0800

    added additional column in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('facebook')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('ids')->nullable();
+            $table->unsignedBigInteger('coach_id')->nullable();
+
+            $table->foreign('coach_id')->references('id')->on('users');
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
