5a25a9acd83e42ef3b0e90172bdf8d7cf9cdc8e8
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-01-15 13:46:30 +0800

    feat(performance): Add PerformanceRecord TypeScript type definition

---
+++ resources/js/types/models/PerformanceRecord.ts
@@ -0,0 +1,14 @@
+export interface PerformanceRecord {
+    id: number;
+    company_id: number;
+    start_date: string;
+    end_date: string;
+    phase: string;
+    no_of_items: number;
+    avg_ads_spent: number;
+    roas: number;
+    rts: number;
+    highlights: number;
+    challenges: number;
+    action_plan: number;
+}
