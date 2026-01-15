968317710173f55033bdd0fcb5dea6aadf056dc3
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-01-15 17:33:35 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::prefix('companies/{company}')->group(function () {
     Route::put('/owners/{id}', [OwnerController::class, 'update'])->name('owners.update');
     Route::delete('/owners/{id}', [OwnerController::class, 'destroy'])->name('owners.delete');
     Route::get('/onboarding', [\App\Http\Controllers\OnboardingChecklistController::class, 'index'])->name('companies.onboarding');
-    Route::put('/onboarding/{onboardingChecklist}', [\App\Http\Controllers\OnboardingChecklistController::class, 'update'])->name('companies.onboarding');
+    Route::put('/onboarding/{onboardingChecklist}', [\App\Http\Controllers\OnboardingChecklistController::class, 'update'])->name('companies.onboarding.update');
     Route::post('/onboarding', [\App\Http\Controllers\CompanyChecklistController::class, 'remark'])->name('remark.store');
     Route::get('/attendance', [\App\Http\Controllers\Company\AttendanceController::class, 'index'])->name('companies.attendance');
     Route::get('performance-records', [PerformanceRecordController::class, 'index'])->name('companies.performance-records.index');
