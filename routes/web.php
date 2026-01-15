352e4e67a901ba150326e8780259f18e19caee5e
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-01-15 13:46:30 +0800

    chore(routes): Update performance record web routes

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,8 @@ Route::prefix('companies/{company}')->group(function () {
     Route::put('/onboarding/{onboardingChecklist}', [\App\Http\Controllers\OnboardingChecklistController::class, 'update'])->name('companies.onboarding');
     Route::post('/onboarding', [\App\Http\Controllers\CompanyChecklistController::class, 'remark'])->name('remark.store');
     Route::get('/attendance', [\App\Http\Controllers\Company\AttendanceController::class, 'index'])->name('companies.attendance');
-    Route::get('performance-records', [PerformanceRecordController::class, 'index'])->name('companies.performance-records.store');
+    Route::get('performance-records', [PerformanceRecordController::class, 'index'])->name('companies.performance-records.index');
+    Route::get('performance-records/create', [PerformanceRecordController::class, 'create'])->name('companies.performance-records.create');
     Route::post('performance-records', [PerformanceRecordController::class, 'store'])->name('companies.performance-records.store');
     Route::post('performance-records/{performance}/', [PerformanceRecordController::class, 'update'])->name('companies.performance-records.update');
     Route::delete('performance-records/{performance}', [PerformanceRecordController::class, 'destroy'])->name('companies.performance-records.destroy');
